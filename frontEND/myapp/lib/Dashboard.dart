587b3dff558b9d606bd3119982fc3ad9b44df769
SiddhanthDubey <sidhanth.du@gmail.com>
2024-02-06 18:22:57 +0530

    flutter frontend

---
+++ frontEND/myapp/lib/Dashboard.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              borderRadius: const BorderRadius.only(
+                bottomRight: Radius.circular(50),
+              ),
+            ),
+            child: Column(
+              children: [
+                const SizedBox(height: 50),
+                ListTile(
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 30),
+                  title: Text('Hello!',
+                      style: Theme.of(context)
+                          .textTheme
+                          .headlineSmall
+                          ?.copyWith(color: Colors.white)),
+                  subtitle: Text('Good Morning',
+                      style: Theme.of(context)
+                          .textTheme
+                          .titleMedium
+                          ?.copyWith(color: Colors.white54)),
+                  trailing: _buildPopupMenuButton(),
+                ),
+                const SizedBox(height: 30)
+              ],
+            ),
+          ),
+          Container(
+            color: Theme.of(context).primaryColor,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius:
+                      BorderRadius.only(topLeft: Radius.circular(200))),
+              child: GridView.count(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                crossAxisCount: 2,
+                crossAxisSpacing: 40,
+                mainAxisSpacing: 30,
+                children: [
+                  itemDashboard('Surveys', CupertinoIcons.play_rectangle,
+                      Colors.deepOrange),
+                  itemDashboard(
+                      'Analytics', CupertinoIcons.graph_circle, Colors.green),
+                  itemDashboard(
+                      'Comments', CupertinoIcons.chat_bubble_2, Colors.brown),
+                  itemDashboard('My Coupons',
+                      CupertinoIcons.money_dollar_circle, Colors.indigo),
+                  itemDashboard(
+                      'About', CupertinoIcons.question_circle, Colors.blue),
+                  itemDashboard(
+                      'Contact', CupertinoIcons.phone, Colors.pinkAccent),
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(height: 20)
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPopupMenuButton() {
+    return PopupMenuButton<String>(
+      icon: Icon(Icons.more_vert, color: Colors.white),
+      onSelected: (value) {
+        if (value == 'logout') {
+          // Perform logout action
+          Navigator.pushReplacementNamed(context, '/login');
+        }
+      },
+      itemBuilder: (BuildContext context) {
+        return [
+          PopupMenuItem<String>(
+            value: 'logout',
+            child: ListTile(
+              leading: Icon(Icons.exit_to_app),
+              title: Text('Logout'),
+            ),
+          ),
+        ];
+      },
+    );
+  }
+
+  Widget itemDashboard(String title, IconData iconData, Color background) =>
+      Container(
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                  offset: const Offset(0, 5),
+                  color: Theme.of(context).primaryColor.withOpacity(.2),
+                  spreadRadius: 2,
+                  blurRadius: 5)
+            ]),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: background,
+                  shape: BoxShape.circle,
+                ),
+                child: Icon(iconData, color: Colors.white)),
+            const SizedBox(height: 8),
+            Text(title.toUpperCase(),
+                style: Theme.of(context).textTheme.titleMedium)
+          ],
+        ),
+      );
+}
